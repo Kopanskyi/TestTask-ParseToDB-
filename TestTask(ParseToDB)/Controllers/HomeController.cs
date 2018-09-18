7ded2f4857512a72f23c1c23fb5a2a2f85a7841c
Taras Kopanskyi <tkopanskyi@gmail.com>
2018-09-19 00:53:51 +0300

    Change UploadFile, add ReadFile

--- TestTask(ParseToDB)/Controllers/HomeController.cs
+++ TestTask(ParseToDB)/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -17,31 +18,64 @@ namespace TestTask_ParseToDB_.Controllers
 
         public HomeController(IHostingEnvironment hostingEnvirontment)
         {
-            environment = hostingEnvirontment;
+            environment = hostingEnvirontment;           
         }
 
         public IActionResult Index()
         {
+            ViewBag.Text = "";
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> UploadFile(IFormFile file)
         {
-            if (file == null || file.Length == 0) return Content("file not selected");
+            if (file == null || file.Length == 0)
+            {
+                ViewBag.Text = "File not selected";
+                return View("Index");
+            }
+
+            var fileExtension = Path.GetExtension(file.FileName);
+
+            if (fileExtension != ".txt")
+            {
+                ViewBag.Text = "Wrong file extension";
+                return View("Index");
+            }
 
             string rootPath = environment.WebRootPath;
             string filePath = $@"{rootPath}\UploadedFiles\{file.FileName}";
-
 
             using (var stream = new FileStream(filePath, FileMode.Create))
             {
                 await file.CopyToAsync(stream);
             }
 
-            //ViewData["FilePath"] = filePath;
-
+            await ReadFile(filePath);
             return View("Index");
+        }
+
+        private async Task ReadFile(string filePath)
+        {
+            byte[] array;
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                array = new byte[stream.Length];
+                await stream.ReadAsync(array, 0, array.Length);
+            }
+
+            string textFromFile = Encoding.Default.GetString(array);
+            ParsingText(textFromFile);
+        }
+
+        private void ParsingText(string textFromFile)
+        {
+            string wordToSearch = string.Format("{0}", Request.Form["wordToSearch"]);
+            string[] sentencesToParse = textFromFile.Split('.');
+
+
         }
     }
 }
